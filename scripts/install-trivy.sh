d1d2019b12ed307a47b86d96ec23a0b056c6b18b
gaurav nailwal <nailwal1emal@gmail.com>
2026-07-31 22:50:23 +0530

    Added SCA scan

--- scripts/install-trivy.sh
+++ scripts/install-trivy.sh
@@ -2,24 +2,22 @@
 
 set -euo pipefail
 
-TRIVY_VERSION="0.65.0"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${SCRIPT_DIR}/versions.sh"
 
 echo "Installing Trivy ${TRIVY_VERSION}..."
 
 WORKDIR=$(mktemp -d)
 cd "$WORKDIR"
 
-DOWNLOAD_URL="https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz"
-
-echo "Downloading from:"
-echo "$DOWNLOAD_URL"
-
-curl -fL -o trivy.tar.gz "$DOWNLOAD_URL"
+curl -fL -o trivy.tar.gz \
+  "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz"
 
 tar -xzf trivy.tar.gz
 
 sudo install -m 755 trivy /usr/local/bin/trivy
 
 echo
-echo "Installed version:"
+echo "Installed Version:"
 trivy --version
---
+++ scripts/versions.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#
+# Security Tool Versions
+#
+
+export GITLEAKS_VERSION="8.24.3"
+export TRIVY_VERSION="0.70.0"
+export CHECKOV_VERSION="3.2.494"
+export SEMGREP_VERSION="1.131.0"
